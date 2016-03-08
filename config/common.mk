ca163b1b81cd2eb49bbc3c1f9269e97984340d38
d34d <clark@cyngn.com>
2016-03-08 09:12:04 -0800

    LLS: Add live lock screen service [3/4]
    Change-Id: I9136e9c9c1413c45aa300f0c92fd69b0c409a052

--- config/common.mk
+++ config/common.mk
@@ -147,7 +147,8 @@ PRODUCT_PACKAGES += \
     LockClock \
     CyanogenSetupWizard \
     CMSettingsProvider \
-    ExactCalculator
+    ExactCalculator \
+    LiveLockScreenService
 
 # Exchange support
 PRODUCT_PACKAGES += \
